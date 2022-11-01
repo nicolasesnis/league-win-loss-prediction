f8f8904e3e8c02cbe95c21dbcda21b99830365e5
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-10-31 18:27:31 -0700

    working with sagemaker

--- Home.py
+++ Home.py
@@ -1,102 +1,105 @@
 import streamlit as st 
-import json
+import plotly.express as px
 from riotwatcher import LolWatcher, ApiError
 import pandas as pd
-import datetime
-# from src.get_live_data import listen_to_game
+from src.sagemaker_util import get_model_endpoint, predict
+from src.get_data import pull_historical_frames
+from src.build_dataset import build_training_dataset, build_match_data
+from src.s3 import list_bucket
+
 
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
+st.title('Historical Game Win/Loss Prediction')
+st.info('Riot does not offer a real-time live game API. This tool allows the win probablity evolution (minute level) for past games only.')
 
-
-# listen_to_game()
-
-
-st.title('League of Legends Win / Loss Prediction tool')
-col1, col2, col3, col4, col5 = st.columns([2,2,2,1,2])
+col1, col2, col3, col4, col5 = st.columns([2,2,1,1,3])
 with col1:
     summoner_name = st.text_input('Enter Your Summoner Name', value='joyeux2cocotier')
 with col2:
-    my_region = st.selectbox('Region', ['na1'])
+    region = st.selectbox('Region', ['na1'])
+
 
 watcher = LolWatcher(st.secrets['riot_api_key'])
-versions = watcher.data_dragon.versions_for_region(my_region)
+versions = watcher.data_dragon.versions_for_region(region)
 
 icons=  watcher.data_dragon.profile_icons(version=versions['n']['profileicon'])
 
+@st.cache
+def get_me(region, summoner_name):
+    me =     watcher.summoner.by_name(region, summoner_name)
+    my_ranked_stats = watcher.league.by_summoner(region, me['id'])
+    my_ranked_stats = [s for s in my_ranked_stats if s['queueType'] == 'RANKED_SOLO_5x5'][0]
+    my_matches = watcher.match.matchlist_by_puuid(region=region, puuid=me['puuid'], queue=420)
+    return me, my_ranked_stats, my_matches
+
 if summoner_name != '':
-    me = watcher.summoner.by_name(my_region, summoner_name)
-    my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-    
+    me, my_ranked_stats, my_matches = get_me(region, summoner_name)
     with col4:
         icon_id_filename = icons['data'][str(me['profileIconId'])]['image']['full']
-        st.image('http://ddragon.leagueoflegends.com/cdn/' + versions['n']['profileicon'] + '/img/profileicon/' + icon_id_filename, width=75)
+        st.image('http://ddragon.leagueoflegends.com/cdn/' + versions['n']['profileicon'] + '/img/profileicon/' + icon_id_filename, width=100)
     with col5:
-        st.write(summoner_name)
-        st.write('Level ' + str(me['summonerLevel']))
-        st.write(my_ranked_stats[0]['tier'] + ' ' + my_ranked_stats[0]['rank']) 
+        st.write(summoner_name + ' | Level ' + str(me['summonerLevel']))
+        tier = my_ranked_stats['tier'] 
+        if tier not in ['SILVER', 'DIAMOND']:
+            st.warning('This tool only works with SILVER or DIAMOND data for now.')
+        division = 'I'
+        st.write(tier + ' ' + my_ranked_stats['rank'] + ' | ' + str(my_ranked_stats['leaguePoints']) + ' points') 
+        wr = my_ranked_stats['wins']  / my_ranked_stats['losses']
+        hot_streak = my_ranked_stats['hotStreak']
+        fresh_blood = my_ranked_stats['freshBlood'] 
+        st.write('Hot Streak: ' + str(hot_streak) + ' | ' + 'Fresh Blood: ' + str(fresh_blood))
+        st.write('Win Rate: ' + str(round(100 * wr, 2)) + '% ('  + str(my_ranked_stats['wins']) + ' wins ; ' + str(my_ranked_stats['losses'])  + ' losses)')
 
+    # Load model with params: Tier, Region
     
     
-    
-    try:
-        current_match = watcher.spectator.by_summoner(region=my_region, encrypted_summoner_id=me['id'])
-        st.write(current_match)
-        match_id = current_match['platformId'] + '_' + str(current_match['gameId'])
-        st.write('Current Match: ' + match_id)
-        start_time = datetime.datetime.fromtimestamp(current_match['gameStartTime'] / 1000).replace(microsecond=0)
-        game_length = datetime.timedelta(seconds=current_match['gameLength'])
-        st.write('Match started at ' + str(start_time) + ' - Time in game so far: ' + str(game_length))
-        st.write('Match ID: ' +match_id)
-    except ApiError as err:
-        if '404' in str(err):
-            st.warning('No current game for this summoner. Pulling last game data.')
-            my_matches = watcher.match.matchlist_by_puuid(region=my_region, puuid=me['puuid'])
-            match_id = my_matches[0]
-            st.write('Last Match')
-    my_matches = watcher.match.matchlist_by_puuid(region=my_region, puuid=me['puuid'])
-    match_id = my_matches[0]
-    st.write(my_matches)
-    match_detail = watcher.match.by_id(region=my_region, match_id=match_id)
-    df = pd.DataFrame(match_detail['info']['participants'])
+    match_id = st.selectbox('Pick a match', my_matches)
     
     
-    df = pd.concat([df.drop(['challenges'], axis=1), df['challenges'].apply(pd.Series).drop(['killingSprees','turretTakedowns' ], axis=1)], axis=1)
-    
-    # Columns Explanation
-    c_dict_path = 'columns.json'
-    with open(c_dict_path, 'r') as f:
-        c_dict = json.load(f)
-    for c in df.columns:
-        if c not in c_dict.keys():
-            c_dict[c] = {'description': '', 'type': str(df[c].dtype)}
-    col = st.selectbox('Features info.', c_dict.keys())
-    st.code(c_dict[col])
-    st.code(df[['championName', col]])
-    
-    
-    st.dataframe(df)
-    
-#     col1, col2 = st.columns(2)
-#     with col1:
+    def check_if_match_is_on_s3(match_id):
+        is_on_s3 =  match_id + '.json'  in list_bucket('s3://league-pred-tool/' + '/'.join(['raw_data', region.upper(), tier, division]) + '/raw_match_context', return_filenames_only=True)
+        return is_on_s3
+    is_on_s3 = check_if_match_is_on_s3(match_id)
+    if not is_on_s3:
         
-#         st.write(participants[0:4])
-#     with col2:
-#         st.write(participants[5:9])
+        pull_historical_frames(region=region.upper(), tier=tier, division=division, number_matches = 1, id = me['id'], match_id = match_id)
+    
+    df = build_match_data(match_id + '.json', '/'.join(['raw_data', region.upper(), tier, division]))
+    # df.fillna(0, inplace=True)
+    without_label = df.fillna(0).drop('blue_team_wins', axis=1)
+    
+    def pred_row(row):
+        input = ','.join([str(i) for i in row])
+        pred =  predict(endpoint_name='silver-endpoint-v7-1', input=input)
+        # blue_win_prediction = int(pred.split(',')[0].split('.')[0])
+        blue_win_win_proba =  1 - float(pred.split(',')[1])
+        return blue_win_win_proba
+    
+    # @st.cache(allow_output_mutation=True)
+    def make_predictions(without_label):
+        df['blue_win_win_proba'] = without_label.apply(lambda x: pred_row(x), axis=1)
+        return df
+    df = make_predictions(without_label)
+    
+    st.write('Actual match winner: blue' if df.blue_team_wins.unique()[0] == 1 else 'Match winner: red')
+    predicted_winner = df.loc[df.minute == 15, 'blue_win_win_proba'].values[0]
+    predicted_winner = 'blue' if predicted_winner > 0.5 else 'red'
+    st.write('Predicted winner at 15mn: ' + predicted_winner)
+    col = st.selectbox('Pick a column', options=df.columns, index=list(df.columns).index('blue_win_win_proba'))
+    import numpy as np
+    df["line_color"] = np.where(df[col]<0, 'red', 'blue')
+    fig = px.bar(df, x="minute", y=col, barmode='group', color='line_color')
+    st.plotly_chart(fig)    
+
+    
+    # if st.button('Pull 1000 matches'):
+    #     path = pull_historical_frames(region=region.upper(), tier=tier, division=division, number_matches = 10000)
+    # if st.button('Process raw data'):
+    #       dataset_name = region + '_' + tier + '_' + division + '.csv'
+    #     build_training_dataset(path='raw_data/NA1/SILVER/I', dataset_name=dataset_name)
     
     
-#     st.dataframe(df)
     
     
+    # st.subheader('Dataset Exploration')
     
-
-
-
-
-# # # Lets get some champions
-# # current_champ_list = watcher.data_dragon.champions(champions_version)
-# # st.write(current_champ_list)
-
-
-
-
-
---
+++ src/build_dataset.py
@@ -0,0 +1,235 @@
+from distutils.command.upload import upload
+from multiprocessing.sharedctypes import Value
+import json
+from src.s3 import list_bucket, read_s3_json_file, upload_df_to_s3
+import pandas as pd 
+import streamlit as st
+
+# 100 = blue side
+
+def compute_blue_lead(teams_dict):
+    blue_lead_dict = {}
+    for team_color, team_stats in teams_dict.items():
+        for stat_obj_name, stat_obj_value in team_stats.items():
+            if type(stat_obj_value) == dict: # individual positions  (jg, mid, bot, etc)
+                for stat_name, stat_value in stat_obj_value.items():
+                    if stat_name not in blue_lead_dict.keys():
+                        blue_lead_dict[stat_name] = 0
+                    if stat_obj_name + '_' + stat_name not in blue_lead_dict.keys():
+                        blue_lead_dict[stat_obj_name + '_' + stat_name] = 0
+                    if team_color == 'blue':
+                        blue_lead_dict[stat_name] += stat_value
+                        blue_lead_dict[stat_obj_name + '_' + stat_name] += stat_value
+                    else:
+                        blue_lead_dict[stat_name] -= stat_value
+                        blue_lead_dict[stat_obj_name + '_' + stat_name] -= stat_value
+            else: # team stats only
+                if stat_obj_name not in blue_lead_dict.keys():
+                    blue_lead_dict[stat_obj_name] = 0
+                if team_color == 'blue':
+                    blue_lead_dict[stat_obj_name] += stat_obj_value
+                else:
+                    blue_lead_dict[stat_obj_name] -= stat_obj_value
+    return {key: value for key, value in sorted(blue_lead_dict.items())}
+            
+    
+def process_participant_frames(participantFrames, team_individual_position_mapping):
+    # First we process all the frames: for each individual position (jg, mid, etc), we compute the blue advantage on all available stats and store it into a dict called "blue_lead_dict"
+    # In addition to the indiviual advantages, we also compute a team advantage because League is a team game
+    frame_data_dict = {'blue': {}, 'red': {}}
+    # First we flatten the raw dict into a simpler dict that will look like:
+    # {
+    #     'blue':
+    #       top' : {
+    #         'armor': 10,
+    #         ...
+    #         }
+    #     }
+    # }
+    for index, participantFrame in participantFrames.items():
+        individual_position = team_individual_position_mapping[str(int(index) - 1)]['individualPosition']
+        team_color = 'blue' if int(index) <= 5 else 'red'
+        frame_data_dict[team_color][individual_position] = {}
+        for stat_categorie, stat_value in participantFrame.items():
+            # Avalailable stats: championStats, currentGold, damageStats, goldPerSecond, jungleMinionsKilled, level, minionsKilled, timeEnemySpentControlled, totalGold, xp
+            if stat_categorie in ['championStats', 'damageStats']: # dict type: 
+                for key, value in stat_value.items():    
+                    frame_data_dict[team_color][individual_position][key] = value
+            elif stat_categorie not in ['position', 'participantId', 'currentGold']: # ignore this one
+                frame_data_dict[team_color][individual_position][stat_categorie] = stat_value
+    
+    # Then we compute all the differences between blue and red teams, for each stat, at role and team levels
+    sorted_blue_lead_dict = compute_blue_lead(frame_data_dict)
+    return sorted_blue_lead_dict
+    
+
+
+def process_new_events_frames(events):
+    # Quick function to keep it DRY
+    def increment_dict(d, team_color, key, value=1):
+        if key not in d[team_color].keys():
+            d[team_color][key] = 0
+        d[team_color][key] += value
+        return d
+        
+    # Same as for stats, we flatten the events dict into a simpler dict distributing the events at team level
+    events_data_dict = {'blue': {}, 'red': {}}
+    for event in events:
+        if 'wardType' in event and event['wardType'] == 'UNDEFINED': # ???
+            continue
+        if 'killerTeamId' in event and event['killerTeamId'] == 300: # when nashor kills herald
+            continue
+        if event['type'] == 'CHAMPION_KILL' and event['killerId'] == 0: # neutral minion executed
+            continue
+        if 'teamId' in event:
+            team_color = 'blue' if event['teamId'] == 100 else 'red'
+        elif 'killerTeamId' in event:
+            team_color = 'blue' if event['killerTeamId'] == 100 else 'red'
+        else:
+            for id_type in ['killerId', 'creatorId', 'participantId']: # the identifier changes depending on the event type
+                if id_type in event.keys():
+                    if event[id_type] == 0:
+                        raise ValueError(event)
+                    team_color = 'blue' if int(event[id_type]) <= 5 else 'red'            
+        if event['type'] in ['WARD_PLACED', 'WARD_KILL']:
+            events_data_dict = increment_dict(events_data_dict, team_color, event['wardType'] + '_' + event['type'])
+        for bt_type in ['shutdownBounty', 'bounty']:
+            if bt_type in event.keys():
+                events_data_dict = increment_dict(events_data_dict, team_color, bt_type.upper() + '_' + event['type'], event[bt_type]) 
+        if event['type'] == 'CHAMPION_SPECIAL_KILL':
+            events_data_dict = increment_dict(events_data_dict, team_color, event['killType'])
+        if event['type'] == 'TURRET_PLATE_DESTROYED':
+            events_data_dict = increment_dict(events_data_dict, team_color, event['laneType'] + '_TURRET_PLATE_DESTROYED')
+        if event['type'] == 'ELITE_MONSTER_KILL':
+            events_data_dict = increment_dict(events_data_dict, team_color, event['monsterType'])
+        if event['type'] == 'BUILDING_KILL':
+            if 'towerType' in event.keys():
+                events_data_dict = increment_dict(events_data_dict, team_color, '_'.join([event['type'], event['towerType'], event['buildingType'], event['laneType']]))
+            else: # inhibitor
+                events_data_dict = increment_dict(events_data_dict, team_color, '_'.join([event['type'], event['buildingType'], event['laneType']]))
+        if event['type'] == 'CHAMPION_KILL' and 'victimId' in event:
+            events_data_dict = increment_dict(events_data_dict, 'blue' if team_color == 'red' else 'red', 'DEATHS')
+        if event['type'] in ['DRAGON_SOUL_GIVEN', 'OBJECTIVE_BOUNTY_FINISH', 'CHAMPION_KILL']:
+            events_data_dict = increment_dict(events_data_dict, team_color, event['type'])
+        if 'assistingParticipantIds' in event.keys(): # CHAMPION_KILL, ELITE_MONSTER_KILL
+            for id in event['assistingParticipantIds']:
+                events_data_dict = increment_dict(events_data_dict, team_color, 'ASSIST_' +  event['type']) 
+        if event['type'] == 'GAME_END':
+            events_data_dict = increment_dict(events_data_dict, 'blue' if event['winningTeam'] == 100 else 'red', 'blue_team_wins') 
+    sorted_blue_lead_dict = compute_blue_lead(events_data_dict)
+    return sorted_blue_lead_dict
+
+
+def process_frame_events(events, prev_blue_events_lead):
+    new_blue_events_lead = process_new_events_frames(events)
+    if prev_blue_events_lead:
+        for key, value in prev_blue_events_lead.items():
+            if key not in new_blue_events_lead.keys():
+                new_blue_events_lead[key] = value
+            else:
+                new_blue_events_lead[key] += value
+    return new_blue_events_lead
+    
+    
+def get_summoners_perf(participants_ids, team_individual_position_mapping, path, game_start_time):
+    out = {'blue': {}, 'red': {}}
+    for i, participant_id in enumerate(participants_ids):
+        championId = team_individual_position_mapping[str(i)]['championId']
+        individualPosition = team_individual_position_mapping[str(i)]['individualPosition']
+        teamColor= 'blue' if i < 5 else 'red'
+        out[teamColor][individualPosition] = {}
+        mastery = read_s3_json_file('s3://league-pred-tool/' + path + '/raw_summoner_champion_mastery_data/' + participant_id + '.json')
+        if mastery != 404:
+            mastery = [m for m in mastery if m['championId'] == championId]
+            if len(mastery) != 0:
+                out[teamColor][individualPosition]['playerChampionLevel'] = mastery[0]['championLevel']
+                out[teamColor][individualPosition]['playerChampionPoints'] = mastery[0]['championPoints']
+                # out[teamColor][individualPosition]['playerDaysSinceLastPlayChamp'] = (pd.to_datetime(game_start_time, unit='ms') - pd.to_datetime(mastery[0]['lastPlayTime'], unit='ms')).days
+                out[teamColor][individualPosition]['playerChampionChestGranted'] = mastery[0]['chestGranted']
+        perf = read_s3_json_file('s3://league-pred-tool/' + path + '/raw_summoner_ranked_data/' + participant_id + '.json')
+        if perf != 404:
+            perf = [p for p in perf if p['queueType'] == 'RANKED_SOLO_5x5']
+            if len(perf) != 0:
+                out[teamColor][individualPosition]['playerWinRate'] = round(perf[0]['wins'] / perf[0]['losses'], 2) if   perf[0]['losses'] > 0 else 1
+                out[teamColor][individualPosition]['playerIsVeteran'] = perf[0]['veteran'] 
+                out[teamColor][individualPosition]['playerIsInactive'] = perf[0]['inactive'] 
+                out[teamColor][individualPosition]['playerIsFreshBlood'] = perf[0]['freshBlood'] 
+                out[teamColor][individualPosition]['playerIsHotStreak'] = perf[0]['hotStreak'] 
+
+    sorted_blue_summoners_lead = compute_blue_lead(out)
+    return sorted_blue_summoners_lead
+
+
+def build_match_data(match_id_fname, path):
+    team_individual_position_mapping = read_s3_json_file('s3://league-pred-tool/' + path + '/raw_match_context/' + match_id_fname)
+    all_roles = [value['individualPosition'] for key, value in team_individual_position_mapping.items()]
+    if 'Invalid' in all_roles:
+        st.warning('Invalid match, please pick another match')
+        return pd.DataFrame()
+    
+    frames = read_s3_json_file('s3://league-pred-tool/' + path + '/raw_match_frames/' + match_id_fname)
+    
+    blue_events_lead = None
+    all_blue_leads = {}
+    for i, frame in enumerate(frames['info']['frames']):
+        if i == 0: # game start
+            game_start_time = frame['events'][0]['realTimestamp']
+            blue_summoners_lead = get_summoners_perf(participants_ids=frames['metadata']['participants'], 
+                                            team_individual_position_mapping=team_individual_position_mapping,
+                                            path=path,
+                                            game_start_time=game_start_time)
+        # convert ts to minute
+        all_blue_leads[int(round(frame['timestamp']/1000/60, 0))] = {
+            'blue_stats_lead': process_participant_frames(frame['participantFrames'], team_individual_position_mapping),
+            'blue_events_lead': process_frame_events(frame['events'], blue_events_lead),
+            'blue_summoners_lead': blue_summoners_lead
+        }
+    df = pd.DataFrame()
+    for minute, dicts in all_blue_leads.items():
+        row = pd.DataFrame()
+        for d_name, d_value in dicts.items():
+            row = pd.concat([row, pd.DataFrame([list(d_value.values())], columns = list(d_value.keys()))], axis=1)
+        row['minute'] = minute
+        df = pd.concat([df, row])
+    df['blue_team_wins'] = df[df['blue_team_wins'].notna()]['blue_team_wins'].unique()[0]
+    
+    # Keep track of all existing columns
+    with open("all_columns.txt", "r") as f:
+        all_columns = f.read().split('\n')
+    for col in df.columns:
+        if col not in all_columns:
+            all_columns.append(col)
+    for col in all_columns:
+        if col not in df.columns:
+            df[col] = None
+    all_columns = sorted(all_columns)
+    with open("all_columns.txt", "w") as outfile:
+        outfile.write("\n".join(all_columns))
+        
+    df = df[all_columns]
+    
+    
+    return df
+                
+                
+def build_training_dataset(path, dataset_name):
+    # Path is like raw_data/NA1/SILVER/I
+    match_id_fnames = list_bucket('s3://league-pred-tool/' + path + '/raw_match_frames', return_filenames_only=True)
+    for i, match_id_fname in enumerate(match_id_fnames):
+        try:
+            df = build_match_data(match_id_fname, path)
+            df.to_csv('raw_data/' + dataset_name, index=None, mode='w' if i == 0 else 'a', header = True if i == 0 else False)
+        except Exception as e:
+            st.warning(e)
+    return 200
+    # upload_df_to_s3(df, 's3://league-pred-tool/input/' + dataset_name)
+        
+            
+            
+            
+  
+        
+# TBD:
+# add timestamp to each frame
+# concat events data overtime
+# add summoner related data
